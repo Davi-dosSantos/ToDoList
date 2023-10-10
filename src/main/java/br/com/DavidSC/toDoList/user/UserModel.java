42fc47a59c14834d9596cf090eab1c2dde91f64d
Davi S Costa <daviita1@hotmail.com>
2023-10-10 10:30:38 -0300

    create bd table user

--- src/main/java/br/com/DavidSC/toDoList/user/UserModel.java
+++ src/main/java/br/com/DavidSC/toDoList/user/UserModel.java
@@ -1,8 +1,26 @@
 package br.com.davidsc.todolist.user;
 
-public class UserModel {
+import java.time.LocalDateTime;
+import java.util.UUID;
 
-    public String username;
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name = "tb_users")
+public class UserModel {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+    private String username;
     public String name;
     public String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
